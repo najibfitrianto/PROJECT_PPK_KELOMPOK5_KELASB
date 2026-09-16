0958c1c78effe08c062215f82175c490fbb9c71a
najibfitrianto <najibfitrianto@gmail.com>
2026-09-16 23:23:53 +0700

    Add Laravel base project

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+<<<<<<< HEAD
+//
+=======
+// TODO: tambahkan endpoint API (mis. untuk versi mobile) jika diperlukan
+>>>>>>> origin/feature/admin-panel
